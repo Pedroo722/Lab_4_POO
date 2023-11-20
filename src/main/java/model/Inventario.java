7509060f07556fe6441231cdbbfe50350c4cc134
ViniciusCavalcantePequeno <144026431+ViniciusCavalcantePequeno@users.noreply.github.com>
2023-11-20 14:17:15 -0300

    Serialização de inventario

--- src/main/java/model/Inventario.java
+++ src/main/java/model/Inventario.java
@@ -3,7 +3,7 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Inventario {
+public class Inventario implements Serializable {
   private List<Produto> estoque;
 
   public Inventario() {
--- src/main/java/model/Serializacao.java
+++ src/main/java/model/Serializacao.java
@@ -6,6 +6,37 @@ public class Serializacao
    public static void main(String [] args) throws FileNotFoundException
    {
         Inventario i = new Inventario();
+        try
+{
+         FileOutputStream fileOut =
+         new FileOutputStream("invetario.ser");
+         ObjectOutputStream out = new ObjectOutputStream(fileOut);
+         out.writeObject(i);
+         out.close();
+         fileOut.close();
+         System.out.printf("Serialized data is saved in invetario.ser");
+      }catch(IOException e)
+      {
+          e.printStackTrace();
+      } 
+
+       
+        FileInputStream fis = new FileInputStream("invetario.ser");
+        try (ObjectInputStream ois = new ObjectInputStream(fis)) {
+            for (Produto produto : i.listarProdutos()) {
+                Produto p2 = (Produto) ois.readObject();
+              }
+            System.out.printf(" AAA");//"%d %s %.2f %d", p2.getIdentificador(), p2.getNome(), p2.getPreco(), p2.getQuantidade());
+        } catch (FileNotFoundException e) {
+            throw e;
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } 
+        
 
     /*
       Produto p = new Produto(123, "bala", 11.5, 15);
